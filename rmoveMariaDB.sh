8e10b63d47c006d035c78a1fee9415377f768aa0
zilin_chen <zilinchen0@gmail.com>
2017-07-22 22:33:37 -0700

    Initial commit

---
+++ rmoveMariaDB.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ps -wef | grep mysql | grep -v grep | awk '{print $2}' | xargs kill -9
+sleep 10
+sudo yum remove -y mysql mysql-devl mysql-server
+sudo yum -y remove mariadb-server mariadb-client
+sudo rm -rf /var/lib/mysql
+sudo rm -rf /var/log/mysql
+sudo rm -rf /etc/my.cnf
+sudo rm -rf /etc/my.cnf.d/
+
